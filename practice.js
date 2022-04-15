cca273a980e4422d94ffdb6c3355a28331d3fc17
jackmonaco20 <jackmonaco20@yahoo.com>
2022-04-15 07:35:45 -0400

    CW

--- practice.js
+++ practice.js
@@ -1,22 +1,20 @@
-function XO(str) {
-    //code here
-  let xs = 0;
-  let os = 0;
-  for( i = 0 ; i < str.length; i++){
-    
-    let newStr = str.toLowerCase
-    if (newStr[i] === 'x'){
-     console.log(xs++);
-    }else if(newStr[i] === 'o'){
-      console.log( os++);
-    }else break;
-    
-  if(xs == os) {
-    return true
-  }else if (xs == 0 && os == 0){
-    return true
-  }else return false
-    
-  }
+arr = [1,2]
+function sumAll(arr) {
+
+    let fullArr = [];
+    let sum = 0;
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+    arr.sort(function(a, b) {
+        return a - b
+    });
+
+    for (let i = arr[0]; i <= arr[1]; i++) {
+        fullArr.push(i);
+    }
+
+    sum = fullArr.reduce(reducer);
+
+    return sum;
 }
-XO('xxOo');
+alert(sumAll)
