7d89898afbf32afc2fe02bee5b8a1d41be03a96e
jackmonaco20 <jackmonaco20@yahoo.com>
2022-04-28 23:40:44 -0400

    another one

--- practice.js
+++ practice.js
@@ -26,3 +26,18 @@
 //    return String(n).split('').reverse().map(Number)
 //  }
 
+////function oddOrEven(array) {
+    //enter code here
+   //initalValue = 0;
+   //let sum = array.reduce(
+ //(previousValue, currentValue) => previousValue + currentValue,
+   //initalValue
+   //);
+   //if(sum % 2 == 0){
+     //return 'even'
+   //}else{
+     //return 'odd'
+   //}
+   
+ //}
+
