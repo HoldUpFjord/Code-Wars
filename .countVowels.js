6c4aee50ac53066fc9fd71be24ceaf0085d6f331
jackmonaco20 <jackmonaco20@yahoo.com>
2022-05-23 08:22:03 -0400

    update

---
+++ .countVowels.js
@@ -0,0 +1,12 @@
+function getCount(str) {
+    let vowelsCount = 0;
+    for(let i = 0; i< str.length; i++){
+      if(str[i] ==='a' ||str[i] === 'e' ||str[i] === 'i' ||str[i] === 'o' ||str[i] === 'u'){
+        return str[i]
+        }
+      
+    }
+    // enter your magic here
+    
+    return vowelsCount;
+  }
