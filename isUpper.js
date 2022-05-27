f3b489deaa3dfc4622b2702278ea8f510b1d8a74
jackmonaco20 <jackmonaco20@yahoo.com>
2022-05-27 00:20:05 -0400

    .

---
+++ isUpper.js
@@ -0,0 +1,7 @@
+var capitals = function (word) {
+    var caps = [];
+    for(var i = 0; i < word.length; i++) {
+      if(word[i].toUpperCase() == word[i]) caps.push(i);
+    }
+    return caps;
+  };
