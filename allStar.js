164642f449578304764c12d61db0262a919a1dde
jackmonaco20 <jackmonaco20@yahoo.com>
2022-06-06 17:56:19 -0400

    .

---
+++ allStar.js
@@ -0,0 +1,11 @@
+function strCount(str, letter){  
+    //code here
+    let count = 0
+    let split = str.split('')
+    for( i = 0; i<split.length ; i++){
+      if(split[i] == letter){
+        count++
+        }
+      }
+    return count
+    
