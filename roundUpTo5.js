b3d47b890c08e2526becd4ba639e9bfc2800e12d
jackmonaco20 <jackmonaco20@yahoo.com>
2022-07-13 22:03:03 -0700

    .

---
+++ roundUpTo5.js
@@ -0,0 +1,17 @@
+function roundToNext5(n){
+    // determine the remainder.
+    let remainder = n % 5
+    
+    // if multiple of 5, returns itself. Includes 0
+    if(remainder === 0 ){
+      return n;
+      
+    // if positive, find difference of n and target. Adds this difference This rounds up
+    }else if(n > 0){
+       let difference = 5 - remainder
+       return n + difference
+      
+      // if negative, turn the remainder positive. Then add it to n.
+    }else if (n < 0){
+      return remainder * -1 + n
+    }
