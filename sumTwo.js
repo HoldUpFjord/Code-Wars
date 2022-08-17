9f22ec5900a4e280a0e3a389434747836913f7df
jackmonaco20 <jackmonaco20@yahoo.com>
2022-08-16 22:58:16 -0400

    .

--- sumTwo.js
+++ sumTwo.js
@@ -1,15 +1,9 @@
 function twoSum(numbers, target) {
-    // target 
-    //empty array for result
-    //loop through numbers
-    //add numbers until equal to target || logic for choosing numbers
-    //return index of numbers in result arr
-    let result = [0];
-    // target = a + b
-    //subract target from first index of array('a')
-    let b = target - numbers[0]
-    //loop to find remainder('b')
-    for(i = 0; i < numbers.length; i++){
-      if( numbers[i]=== b){
-        result.push(i) 
-      }
+    let seen = new Map();
+    for (let i = 0; i < numbers.length; i++) {
+      let x = numbers[i], y = target - x;
+      if (seen.has(y))
+        return [seen.get(y), i];
+      seen.set(x, i);
+    }
+  }
