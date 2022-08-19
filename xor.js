2008c71decbec65837eeeffc2ad83e339b8aaeb3
jackmonaco20 <jackmonaco20@yahoo.com>
2022-08-18 21:43:43 -0400

    .

---
+++ xor.js
@@ -0,0 +1,8 @@
+function xor(a, b) {
+    // TODO: Program Me
+    if( a === true && b === false){
+      return true
+    } else if(  a === false  && b === true){
+      return true
+    }else return false
+  }
