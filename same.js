2dce3365ac1b6660fc66433bbca3e996fc98d671
jackmonaco20 <jackmonaco20@yahoo.com>
2022-08-25 20:50:08 -0400

    .

---
+++ same.js
@@ -0,0 +1,7 @@
+function comp(a1, a2){
+    if (a1==null || a2==null) return false;
+    a1=a1.sort(function(a,b){return b<a});
+    a2=a2.sort(function(a,b){return b<a});
+    for (var i in a1) if (Math.pow(a1[i],2)!=a2[i]) return false;
+    return true;
+  }
