441c282dc45d410f8139888d3a010c76c5b43321
jackmonaco20 <jackmonaco20@yahoo.com>
2022-09-11 16:17:26 -0400

    .

---
+++ maxMultiple.js
@@ -0,0 +1,14 @@
+function maxMultiple(divisor, bound){
+    //your code here
+    for(i = bound; i < bound + 1; i--){
+      if(i % divisor != 0){
+        null
+      }else if (i % divisor === 0){
+        
+        return i
+        break
+        
+      }
+      
+    }
+  }
