cfda4129f737e4e79e180f8075206294641ba53f
jackmonaco20 <jackmonaco20@yahoo.com>
2022-09-16 21:54:13 -0400

    solved 2/4 test cases

---
+++ queue.js
@@ -0,0 +1,18 @@
+function queueTime(customers, n) {
+    //TODO
+    //if customers array is equal to zero
+    if(customers.length === 0 ){
+      return  0
+    //if n = 1, return sum array
+    }else if( n === 1){
+      return customers.reduce((a,b) => 
+        a+b,
+          0,
+      );
+    }else if()
+    
+   //if customers[0] is greater than the sum of the rest of the arr, return customers[0]
+   //
+    //if n > sum of customers, return the greatest numer in arr
+    
+  }
