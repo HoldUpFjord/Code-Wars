ee666f1f00791e276a8e1906a04c366420f74130
jackmonaco20 <jackmonaco20@yahoo.com>
2022-09-25 22:44:21 -0400

    .

---
+++ sumNeg.js
@@ -0,0 +1,17 @@
+function countPositivesSumNegatives(input) {
+    if (input == null || input.length == 0)
+      return [];
+    
+    var positive = 0;
+    var negative = 0;
+    
+    for (var i=0, l=input.length; i<l; ++i)
+    {
+      if (input[i] > 0)
+        ++ positive;
+      else
+        negative += input[i];
+    }
+    
+    return [positive, negative];
+}
