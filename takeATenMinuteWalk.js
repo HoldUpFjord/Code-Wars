d6aaac4aabe78e47220b44db8ea877931605c9c7
jackmonaco20 <jackmonaco20@yahoo.com>
2022-09-27 10:19:48 -0400

    .

--- takeATenMinuteWalk.js
+++ takeATenMinuteWalk.js
@@ -1,31 +1,39 @@
 function isValidWalk(walk) {
-    //insert brilliant code here
-    //We need to count each elements in array. Walk.length
-    // Two variables for each axis.NS: N = +1, S = -1. WE: W = -1, E = + 
-    //for loop, conditionals for iterating nsew
     
-    //empty array to contain num values for cardinal directions
-    let directions =[]
-    //loop through walk array, icrement based upon direction given
+    //We need to count each elements in array. Walk.length
+    // Two variables for each axis.NS: N = +1, S = -1. WE: W = -1, E = +1
+    //for loop, conditionals for iterating cardinals
+    
+    //empty array to contain num values for each axis directions
+    let directionsX =[]
+    let directionsY = []
+    //loop through walk array, increment based upon direction given
     for( i = 0; i < walk.length; i++){
       //empty increment variable
-      let cardinals = 0;
+      let xAxis = 0;
+      let yAxis = 0;
      
-      if( walk[i] === 'n' || walk[i] === 'e') {
-        cardinals ++
-      } else if( walk[i] === 's' || walk[i] === 'w' ) {
-        cardinals --
-      } 
+      if( walk[i] === 'n' ) {
+        xAxis ++
+      } else if( walk[i] === 's') {
+        xAxis --
+      } else if( walk[i] === 'w'){
+        yAxis --
+      }else if( walk[i] === 'e'){
+        yAxis ++
+        }
       //push transformed walked array to a empty arr as numbers
-      directions.push(cardinals)
+      directionsX.push(xAxis);
+      directionsY.push(yAxis)
       
     }
-    //sum array
-    let count = directions.reduce((a,b) => a+b)
+    //sum array's as two seperate axis. Prevents error of inputting (n,w,n,w,n,w ) = 0 as n = 1 & w = -1. Computationally that would read as arriving at the correct place
+    let countX = directionsX.reduce((a,b) => a+b)
+    let countY = directionsY.reduce((a,b) => a+b)
     
     let blocks = walk.length
-    //if the amount of blocks is 10, and you're back where you started: count being 0
-    if(blocks === 10 && count === 0){
+    //if the amount of blocks is 10, and you're back where you started on x-axis(= 0) and y-axis(= 0 ): true
+    if(blocks === 10 && countX === 0 && countY === 0 ){
       return true
     } else {
       return false
