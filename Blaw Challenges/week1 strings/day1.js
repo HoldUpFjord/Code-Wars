925ee6748863acdce2ee60cc4d6179ea0f23943a
jackmonaco20 <jackmonaco20@yahoo.com>
2022-10-02 22:56:11 -0400

    .

---
+++ Blaw Challenges/week1 strings/day1.js
@@ -0,0 +1,35 @@
+let string = "PR0-T1P #hqB: 1T'5 N1C3 T0 5AY H3770." 
+// implement fix function -> 
+"PRO-TIP #498: IT'S NICE TO SAY HELLO."
+
+let autoCorrect = {
+    '0':'O',      
+    '1' : 'I',     
+    '2' :'Z',     
+    '3' : 'E',     
+    '4' : 'h',      
+    '5' : 'S',
+    '6' : 'G',
+    '7' : 'L',
+    '8' : 'B',
+    '9' : 'q'
+}
+function week1(string) {
+    let autoCorrect = {
+        '0':'O',      
+        '1' : 'I',     
+        '2' :'Z',     
+        '3' : 'E',     
+        '4' : 'h',      
+        '5' : 'S',
+        '6' : 'G',
+        '7' : 'L',
+        '8' : 'B',
+        '9' : 'q'
+    }
+    //for(i = 0; i < string.length; i++){
+       //return Object.keys(autoCorrect)
+   // }
+    
+}
+console.log(Object.values(autoCorrect))
