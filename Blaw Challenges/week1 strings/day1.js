52b72db8e92e287b3c7906f07211bf95890b82d6
jackmonaco20 <jackmonaco20@yahoo.com>
2022-10-05 22:59:51 -0400

    .

--- Blaw Challenges/week1 strings/day1.js
+++ Blaw Challenges/week1 strings/day1.js
@@ -2,6 +2,11 @@ let string = "PR0-T1P #hqB: 1T'5 N1C3 T0 5AY H3770."
 // implement fix function -> 
 "PRO-TIP #498: IT'S NICE TO SAY HELLO."
 
+//.split the string
+//loop through split string
+//if ([i]) is equal to any key/value in autoCorrect, swap them
+//return joined arr
+
 let autoCorrect = {
     '0':'O',      
     '1' : 'I',     
