80850d39f10422ab583180efdfe98d85dbd16373
jackmonaco20 <jackmonaco20@yahoo.com>
2022-10-08 15:23:31 -0400

    .

--- persistentBugger.js
+++ persistentBugger.js
@@ -1,34 +1,12 @@
-function persistence(num){
-  //doesn't work for 999.
-  let numString = num.toString();
-  let count = 0
-  if( numString.split('').length === 1){
-    console.log(count)
-  }else{
-     //1.) for loop: i = result. 
-    // 1.)convert num to string.
-    // 2.) split numString
-    //3.) convert numString back to int
-    // 4.) reduce numSplit 
-   //5.) return output of step 4. to loop
-  for(i = 0 ; i >=0; i++){
-     count ++
-     let stringInt = []
-     let stringSplit = num.toString().split('')
-     for( i = 0; i < stringSplit.length; i++){
-       if( numString.split('').length === 1){
-        console.log(count)
-    }else{
-       stringInt.push(parseInt(stringSplit[i], 10))
-     }
-      console.log(stringInt.reduce((a,b) => a*b))
-      return
-     }
-   }
-    
+function persistence(num) {
+  var times = 0;
+  
+  num = num.toString();
+  
+  while (num.length > 1) {
+    times++;
+    num = num.split('').map(Number).reduce((a, b) => a * b).toString();
   }
- 
   
-  
+  return times;
 }
-persistence(25)
