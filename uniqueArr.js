0a503bc3129cdc74c66b4f686ebbc06061ad8cdd
jackmonaco20 <jackmonaco20@yahoo.com>
2022-10-09 18:26:08 -0400

    .

---
+++ uniqueArr.js
@@ -0,0 +1,5 @@
+function uniqueCombos(arr){
+    //set -> unique value
+    //map [1,2], [4], [5,6] -> 2,1,2: unique values per array. Using set
+    return arr.map(subArr => new Set(subArr).size).reduce((a,c) => a*c,1)
+  }
