265111ea5b63be2314580170f460d6b590c972b5
jackmonaco20 <jackmonaco20@yahoo.com>
2022-10-21 13:50:03 -0400

    .

---
+++ chromosome.js
@@ -0,0 +1,8 @@
+function chromosomeCheck(sperm) {
+    //conditonal logic
+      if(sperm === 'XY') {
+        return "Congratulations! You\'re going to have a son."
+      }else if( sperm === 'XX'){
+        return "Congratulations! You\'re going to have a daughter."
+      }
+  }
