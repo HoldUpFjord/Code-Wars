d2787adabc8018b9acd9de2c9e3ca2b1092e36d3
jackmonaco20 <jackmonaco20@yahoo.com>
2022-10-23 15:13:04 -0400

    .

--- persistentBugger.js
+++ persistentBugger.js
@@ -1,23 +1,34 @@
-function persistence(num) {
-    //code me
-   //1.) for loop: i = result. 
-     // 1.)convert num to string.
-     // 2.) split numString
-     //3.) convert numString back to int
-     // 4.) reduce numSplit 
-    //5.) return output of step 4. to loop
-   
-   /*for(i = 0 ; i >=0; i++){
-     
-      let stringInt = []
-      let stringSplit = num.toString().split('')
-      for( i = 0; i < stringSplit.length; i++){
-        stringInt.push(parseInt(stringSplit[i], 10))
-      }
-       return stringInt.reduce((a,b) => a*b)
-      
-   }*/ 
-   let string = num.toString();
-   return string.split('').length
-   
- }
+function persistence(num){
+  //doesn't work for 999.
+  let numString = num.toString();
+  let count = 0
+  if( numString.split('').length === 1){
+    console.log(count)
+  }else{
+     //1.) for loop: i = result. 
+    // 1.)convert num to string.
+    // 2.) split numString
+    //3.) convert numString back to int
+    // 4.) reduce numSplit 
+   //5.) return output of step 4. to loop
+  for(i = 0 ; i >=0; i++){
+     count ++
+     let stringInt = []
+     let stringSplit = num.toString().split('')
+     for( i = 0; i < stringSplit.length; i++){
+       if( numString.split('').length === 1){
+        console.log(count)
+    }else{
+       stringInt.push(parseInt(stringSplit[i], 10))
+     }
+      console.log(stringInt.reduce((a,b) => a*b))
+      return
+     }
+   }
+    
+  }
+ 
+  
+  
+}
+persistence(25)
