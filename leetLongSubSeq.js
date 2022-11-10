8ec04356ee9dcf1a5e6bbb24eae82a535c14b07e
jackmonaco20 <jackmonaco20@yahoo.com>
2022-11-09 21:11:38 -0500

    .

--- leetLongSubSeq.js
+++ leetLongSubSeq.js
@@ -1,7 +1,9 @@
 function longSubSeq(nums, queries){
     for(i = 0; i < queries.length; i++){
       for(j = 0; j < nums.length; j++){
-        console.log(nums.filter(num => num > queries[i]))
+        if(nums.filter(num => num < queries[i] === [])){
+            return [0]
+        }
       }
     }
   } 
