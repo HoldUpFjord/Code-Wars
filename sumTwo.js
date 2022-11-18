efab7113d7a02b133e1b0b67243a70b2761b160b
jackmonaco20 <jackmonaco20@yahoo.com>
2022-11-18 14:45:09 -0500

    .

--- sumTwo.js
+++ sumTwo.js
@@ -1,9 +1,27 @@
 function twoSum(numbers, target) {
+  //bruteForce: nested for loop
+    //if nums[i] + nums[j] = target return true
+    let result = []
+    for(i = 0; i < nums.length; i++){
+        for(j = i + 1; j < nums.length; j++){
+            if(nums[i] + nums[j] === target){
+                result.push(nums.indexOf(nums[i])) 
+                result.push(nums.indexOf(nums[j]))
+            }
+        }
+    }
+  return result
+};
+
+  
+  /*
+
     let seen = new Map();
     for (let i = 0; i < numbers.length; i++) {
       let x = numbers[i], y = target - x;
       if (seen.has(y))
         return [seen.get(y), i];
       seen.set(x, i);
-    }
+    }*/
+
   }
