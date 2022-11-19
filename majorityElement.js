f821042eb1648c8215b4b5eaaa3544c34d8ce7e5
jackmonaco20 <jackmonaco20@yahoo.com>
2022-11-19 14:11:26 -0500

    .

--- majorityElement.js
+++ majorityElement.js
@@ -13,3 +13,23 @@ Output: 2*/
 //brute force: nested for loops
 //Linear:hashmaps 
 //log:sort
+
+
+function majEl(arr){
+    //incomplete: able to count, unable to find if maj element isnt arr[0]
+    let countA = 0
+    let countB = 0
+    for( i = 0; i < arr.length; i++){
+      if (arr[i] === arr[0]){
+        countA ++
+      } else{
+        countB ++
+      }
+      
+    }
+    if(countA > countB){
+      return arr[0]
+    }else{
+      return 
+    }
+    
