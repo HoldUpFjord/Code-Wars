36dec808e0a596f73d7f6a3549511546774e6f38
jackmonaco20 <jackmonaco20@yahoo.com>
2022-11-22 23:46:42 -0500

    .

---
+++ introToHash.js
@@ -0,0 +1,8 @@
+function findJewels(stones, jewels){
+    const map = {}
+    let count = 0
+  //input jewels into map object with value of true
+    for(const jewel of jewels){
+      map[jewel] = true
+    }
+}
