6f3308439250430d8800261c104010f609d30a70
jackmonaco20 <jackmonaco20@yahoo.com>
2022-12-01 13:37:12 -0500

    .

--- leetcode.parathesis.js
+++ leetcode.parathesis.js
@@ -1,5 +1,41 @@
 ▶︎
-function paranthesis(s) {…};
+
+function paranthesis(s) {
+    // input s will always be a string
+    //input s only comprised of bracket variations
+    // A valid string is comprised of a series of open, close brackets of the same type.
+    //Automatic end case, no open parathesis/brackets in the first position
+     if(s[0] ==='(' || s[0] === '[' || s[0] === '{'  ){
+            for( i = 0; i < s.length; i++){
+        
+        //conditional to exclude starting point on odd/closed index's
+            if(i === 0 ||  i % 2 == 0){
+                  //conditonals conditional logic for false return values
+                  if(s[i] === '(' && s[i+1] != ')' ){
+                      console.log('case1')
+                      return false
+                      
+                  }
+                  else if (s[i] === '[' && s[i+1] != ']' ){
+                      console.log('case2')
+                      return false
+                  }
+                  else if (s[i] === '{' && s[i+1] != '}' ){
+                      console.log('case3')
+                      return false
+                  }
+                  //else return true
+                  else return true
+            }
+          }
+    //loop through string
+    
+    }
+  else{
+    return false
+  }
+};
 
 console.log((paranthesis('()')), 'true')
-
+console.log((paranthesis('(){}[]')), 'true')
+console.log((paranthesis("{[]}")))
