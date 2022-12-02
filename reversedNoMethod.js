4cb9a913defd3338a1dfeb144c8cf829c906ede1
jackmonaco20 <jackmonaco20@yahoo.com>
2022-12-02 16:46:27 -0500

    .

---
+++ reversedNoMethod.js
@@ -0,0 +1,13 @@
+function reverse(str){
+    // empty result str
+    let result = ''
+    // for loop -> i = str.length, i--
+    for( i = str.length-1; i >= 0 ; i--){
+      // push str[i] result
+      result += str[i]
+      }
+    
+    return result
+    
+  }
+  
