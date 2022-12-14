b621a0ee97a60da4c3cfef7617a773b14018193f
jackmonaco20 <jackmonaco20@yahoo.com>
2022-12-14 14:59:29 -0500

    .

--- sumTwo.js
+++ sumTwo.js
@@ -1,12 +1,16 @@
 function twoSum(numbers, target) {
   //bruteForce: nested for loop
     //if nums[i] + nums[j] = target return true
-    // does not work if two numbers are duplicates: i.e [3,3] , 6
+    
     let result = []
     for(i = 0; i < nums.length; i++){
-        for(j = i + 1; j < nums.length; j++){
-            if(nums[i] + nums[j] === target){
-                result.push(nums.indexOf(nums[i])) 
+      for(j = i + 1; j < nums.length; j++){
+        if(nums[i] + nums[j] === target){
+            if(nums[i] === nums[j]){
+                result.push(nums.indexOf(nums[i]))
+                result.push(nums.indexOf(nums[j], i + 1))
+            }else{
+                result.push(nums.indexOf(nums[i]))
                 result.push(nums.indexOf(nums[j]))
             }
         }
