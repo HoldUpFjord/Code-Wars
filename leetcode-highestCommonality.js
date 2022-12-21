a3d35b3b5035d095696d1d405dd91cbf7d3b904f
jackmonaco20 <jackmonaco20@yahoo.com>
2022-12-20 19:14:23 -0500

    .

---
+++ leetcode-highestCommonality.js
@@ -0,0 +1,7 @@
+if (strs === undefined || strs.length === 0) { return ''; }
+    
+return strs.reduce((prev, next) => {
+    let i = 0;
+    while (prev[i] && next[i] && prev[i] === next[i]) i++;
+    return prev.slice(0, i);
+});
