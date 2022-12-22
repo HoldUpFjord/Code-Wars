2008cdbf33e12d55b43fd1c47161d95e35c8dff1
jackmonaco20 <jackmonaco20@yahoo.com>
2022-12-22 01:06:27 -0500

    .

---
+++ fibonacciIndex.js
@@ -0,0 +1,12 @@
+// brute force
+function findonacci(n){
+    // starts from 0 & 1
+      //first two index
+    let arr = [0,1]
+    //start for loop @ index 2
+    for(i = 2 ; i <= n ; i++){
+        //
+     arr[i] = arr[i-1] + arr[i-2]
+      }
+      return arr[n]
+}
