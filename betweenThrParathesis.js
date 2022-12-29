c3bb95e6a94585e343141582fabe2ed02ff22d22
jackmonaco20 <jackmonaco20@yahoo.com>
2022-12-28 23:40:59 -0500

    .

--- betweenThrParathesis.js
+++ betweenThrParathesis.js
@@ -1,3 +1,19 @@
+//proper way 
+function removeParentheses(s){
+    let result = "";
+    let count = 0;
+    for (let letter of s){
+      if (letter == "(") count += 1;
+      if (count == 0) result += letter;
+      if (letter == ")") count -= 1
+    }
+    return result
+  }
+
+
+
+
+
 function removeParentheses(s){
     //input is a string
     //returning string 
