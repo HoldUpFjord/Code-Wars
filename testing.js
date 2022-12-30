70b0e8110b8e403abeb87de069869d32b8027a8b
jackmonaco20 <jackmonaco20@yahoo.com>
2022-12-30 01:46:14 -0500

    .

--- testing.js
+++ testing.js
@@ -1,25 +1,5 @@
-var number=function(array){
-    //your awesome code here
-    // Takin in an arr of str's
-    //returning a an arr which has each k/v pair as a str
-      
-      //conditonal for if array is empty
-    if(!array.length){
-      return array;
-    } else {
-      //create object
-      let count = {};
-      let result = [];
-      //for ele of arr loop
-      for(const letter of array){
-      //if obj[arr] exists +1 or =1 ternary
-        count[letter] = count[letter] ? count[ele]+1 : 1;
-        result.push(count[letter])
-      }
-      
-      return result
-     
-        
-      
-     }
-  }
+var number = function(array) {
+    return array.map(function (line, index) {
+      return (index + 1) + ": " + line;
+    });
+} 
