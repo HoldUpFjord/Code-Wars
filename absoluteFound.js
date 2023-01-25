fed2ce4e31b56ea43d01de5f20b88632f10d3662
jackmonaco20 <jackmonaco20@yahoo.com>
2023-01-24 23:52:18 -0500

    .

--- absoluteFound.js
+++ absoluteFound.js
@@ -1,20 +1,26 @@
-//total = 0
-let total = 0
-//for loop 
-let baseP = parseInt(p)
-
-for( i = 0; i <= p; i++){
-//math.(.pow(nString[i], p)) += total
-let power = Math.pow(splitN[i], baseP) 
-console.log(total += power)
-  baseP ++
-}
-
- if( total % n === 0){
-   return total / n
- } else{
-   return -1
- }
-//else return -1
-
-}
+function digPow(n, p){
+    // ...
+    //n.toString
+    let stringN = n.toString()
+    // nSting.split
+    let splitN = stringN.split('')
+    //total = 0
+    let total = 0
+    //for loop 
+    let baseP = parseInt(p)
+    
+    for( i = 0; i <= p; i++){
+  //math.(.pow(nString[i], p)) += total
+    let power = Math.pow(splitN[i], baseP) 
+    console.log(total += power)
+      baseP ++
+    }
+    
+     if( total % n === 0){
+       return total / n
+     } else{
+       return -1
+     }
+    //else return -1
+  
+  }
