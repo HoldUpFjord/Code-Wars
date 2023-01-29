b3f3644353636bb98b1e08171d1d8e52b1063408
jackmonaco20 <jackmonaco20@yahoo.com>
2023-01-28 23:51:19 -0500

    .

---
+++ absoluteFound.js
@@ -0,0 +1,20 @@
+//total = 0
+let total = 0
+//for loop 
+let baseP = parseInt(p)
+
+for( i = 0; i <= p; i++){
+//math.(.pow(nString[i], p)) += total
+let power = Math.pow(splitN[i], baseP) 
+console.log(total += power)
+  baseP ++
+}
+
+ if( total % n === 0){
+   return total / n
+ } else{
+   return -1
+ }
+//else return -1
+
+}
