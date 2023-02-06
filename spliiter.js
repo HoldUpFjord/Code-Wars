6aaec31ff3b73d0b1fb64574df9839566f84e512
jackmonaco20 <jackmonaco20@yahoo.com>
2023-02-06 16:03:33 -0500

    brute force

---
+++ spliiter.js
@@ -0,0 +1,21 @@
+//Brute force
+function getMiddle(s){
+   //var for split string
+   let splitString = s.split('')
+    // if even
+  if(s.length % 2 === 0){
+    //return string.split  arr.length/2 and arr.length/2 + 1
+    let result = []
+    result.push(splitString[Math.floor(s.length/2) - 1])
+    result.push(splitString[Math.floor(s.length/2)])
+    return result.join('')
+  }
+   
+   
+    // if odd
+  if(s.length % 2 != 0){
+    return splitString[Math.floor(s.length/2)]
+     return string.split(array.length/2)
+  }
+ 
+}
