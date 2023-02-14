640f2a9412fa8106ea6f265b87bda4a00affefee
jackmonaco20 <jackmonaco20@yahoo.com>
2023-02-14 14:10:31 -0500

    arrow function refactor

--- objects.js
+++ objects.js
@@ -1,4 +1,8 @@
+//Function
 function animal(obj){
   
     return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
 }
+
+//Arrow function 
+const animal = (obj) => `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
