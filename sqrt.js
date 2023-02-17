28025555d64064548ba748570ceab5000d74aeb9
jackmonaco20 <jackmonaco20@yahoo.com>
2023-02-16 22:34:30 -0500

    .

---
+++ sqrt.js
@@ -0,0 +1,17 @@
+function sqrt (n) {
+  
+  
+    //PREP
+      // taking in a num
+      //Returning a booleon 
+      //
+      //conditional
+  let sqr = Math.sqrt(n)
+  
+    if(sqr - Math.floor(sqr) !== 0){
+    return false
+    } else {
+    return true
+    }
+          //else return false
+  }
