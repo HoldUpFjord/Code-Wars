374e728ec18920fadd12ad11717a821dc8388ac5
jackmonaco20 <jackmonaco20@yahoo.com>
2023-02-19 22:45:51 -0500

    .

---
+++ easyPeasy.js
@@ -0,0 +1,12 @@
+function updateLight(current) {
+  
+    //your code here!
+    if(current === 'green'){
+      return 'yellow'
+    } else if( current === 'yellow'){
+      return 'red'
+    } else if( current === 'red'){
+      return 'green'
+    }
+  
+  }
