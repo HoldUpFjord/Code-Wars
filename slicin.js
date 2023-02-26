aedb9f7fa363ccf6bdefbb70bb29a39c9674a202
jackmonaco20 <jackmonaco20@yahoo.com>
2023-02-26 17:03:04 -0500

    brute force correct

--- slicin.js
+++ slicin.js
@@ -3,12 +3,16 @@ function arrayDiff(a, b) {
     //return a.filter(i => i !b.includes(i))
     
 //brute force
-    for(i = 0; i < a.length; i++){
-        for(j = 0; j < b.length; j++){
-          if (b[j] === a[i]){
-            a.splice(a[i-1], a[i])
-          }
-        }
+function arrayDiff(a, b) {
+  if(b.length==0 || a.length==0)
+    return a;
+  for(let i =0;i<a.length;i++){
+    for(let j=0;j<b.length;j++){
+      if(a[i]==b[j]){
+        a.splice(i,1);
+        i--;
       }
-      return a
+    }
   }
+  return a;
+}
