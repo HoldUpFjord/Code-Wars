533a97ae43afe324d24be27bf26e015744e8f549
jackmonaco20 <jackmonaco20@yahoo.com>
2023-03-05 04:09:07 -0500

    ternary

--- divyUp.js
+++ divyUp.js
@@ -20,3 +20,10 @@ function arithmetic(a, b, operator){
     'multiply': a * b,
     'divide'  : a / b
   }[operator]);
+
+  //ternary 
+  const arithmetic = (a, b, operator) => {
+    return  ( 
+      operator === "add" ? a + b : operator === "subtract" ? a - b : operator === "multiply" ? a * b : a / b
+    )
+   }
