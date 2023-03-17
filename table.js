35725d864b4d140c0033c250af58dbcf09e4ac8e
jackmonaco20 <jackmonaco20@yahoo.com>
2023-03-16 22:05:14 -0400

    .

---
+++ table.js
@@ -0,0 +1,9 @@
+const multiTable = (number) => {
+    let table = '';
+    
+    for(let i = 1; i <= 10; i++) {
+      table += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`; 
+    }
+  
+    return table;
+  }
