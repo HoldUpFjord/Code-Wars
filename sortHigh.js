28a1c5e455802dac259374be9296efcc354666d1
jackmonaco20 <jackmonaco20@yahoo.com>
2023-03-18 17:15:35 -0400

    bubble sort

--- sortHigh.js
+++ sortHigh.js
@@ -10,3 +10,24 @@ function largestPairSum (numbers) {
      let sortedArr = numbers.sort((a,b) => b-a)
      return sortedArr[0] + sortedArr[1]
   }
+
+  //bubble sort
+  function largestPairSum (numbers) {
+    //TODO: Write your Code here
+    //input an array of nums
+    //output: single num which is the sum of the two largest nums in array
+    
+    //bubble sort(?)
+      // for loop
+    for(i = 0; i < numbers.length; i++){
+      for(j = 0; j < numbers.length ; j++){
+        if( numbers[j] < numbers[j+1] ){
+          let temp = numbers[j]
+          numbers[j] = numbers[j+1]
+          numbers[j+1] = temp
+        }
+      }
+    
+    }
+      return numbers[0] + numbers[1]
+  };
