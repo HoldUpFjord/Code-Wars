4e1bcaa147927be2334ce83a14d67d2c85467811
jackmonaco20 <jackmonaco20@yahoo.com>
2023-03-19 19:03:43 -0400

    quicksort

--- sortHigh.js
+++ sortHigh.js
@@ -17,3 +17,32 @@ function largestPairSum (numbers) {
     }
       return numbers[0] + numbers[1]
   };
+
+  //quicksort
+  function largestPairSum (numbers) {
+  
+    if (numbers.length <= 1){
+      return numbers
+    }
+    
+     const pivot = numbers[0];
+     const left = [];
+     const right = [];
+  
+     let count = 0
+     for(let i = 1 ; i < numbers.length ;  i++){
+      count ++
+      if(numbers[i] >  pivot){
+        left.push(numbers[i]);
+        } else{
+          right.push(numbers[i]);
+      }
+            
+      
+    }
+       
+   return largestPairSum(left).concat(pivot, largestPairSum(right));
+  };
+  const numbers = [1,2,3,4,6,-1,2]
+  const sortedNumbers = largestPairSum(numbers);
+  console.log((sortedNumbers[0]+sortedNumbers[1]), );
