66fa2a62469ac3358d566de3aebb685f49e5eb0f
jackmonaco20 <jackmonaco20@yahoo.com>
2023-03-21 19:32:44 -0400

    leetcode easy

---
+++ lengthLastLC.js
@@ -0,0 +1,15 @@
+var lengthOfLastWord = function(s) {
+    if(s.length === 1){
+       return s.length
+      } else  {
+    const split = s.split(' ')
+    console.log(split.length)
+    for(i = split.length-1; i >= 0 ; i--){
+        if(split[i].length > 0){
+            let removeSpaces = split[i].split('').filter(x => x != ' ').join('')
+            // console.log(removeSpaces.length)
+            return split[i].length
+            } 
+    }
+  }
+};
