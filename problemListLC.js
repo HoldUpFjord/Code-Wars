1f12a620115ae03ef6b436ab4bee553d5098fe34
jackmonaco20 <jackmonaco20@yahoo.com>
2023-04-21 16:15:19 -0400

    typescript solution

--- problemListLC.js
+++ problemListLC.js
@@ -13,3 +13,15 @@ var reduce = function(nums, fn, init) {
     }
     return acc
 };
+//typescript version
+// //type Fn = (accum: number, curr: number) => number;
+
+// function reduce(nums: number[], fn: Fn, init: number): number {
+//     let val = init;
+
+//     for (const num of nums) {
+//         val = fn(val, num);
+//     }
+
+//     return val;
+// }
