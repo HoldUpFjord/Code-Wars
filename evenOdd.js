eb130d3385be7b1f7a224071079c2ae8cd79ed1e
jackmonaco20 <jackmonaco20@yahoo.com>
2023-04-25 12:27:13 -0400

    .

---
+++ evenOdd.js
@@ -0,0 +1,11 @@
+function evenOrOdd(number) {
+    // input is a single number
+    //output is a string;  "Even" if even num or "Odd" if odd num
+    // since its binary, a single if/else statement will do. 
+      if(number % 2 === 0 ){
+        return 'Even'
+      } else {
+        return "Odd"
+      }
+    
+  }
