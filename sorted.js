c0d8c1aa057b062131c1fbe63f79f585ec607120
jackmonaco20 <jackmonaco20@yahoo.com>
2023-04-27 19:25:51 -0400

    .

--- sorted.js
+++ sorted.js
@@ -1,17 +1,18 @@
-//almost works
+// works
 function isSortedAndHow(array) {
     //takes in array of ints
     //returns a string
       //one if array is asscending order, two if descending order, three if not ordered
-    
+    let ascending = array.slice().sort((a,b) => a-b)
+    let descending = array.slice().sort((a,b) => b-a)
     //if array equals array,sort ascending return one
-     if(array === array.sort((a,b) => a - b)){
+  
+     if(array === ascending){
        return 'yes, ascending'
-     } else if(array === array.sort((a,b) => b - a)){
+     }  else if(array === descending){
       return 'yes, descending'
     } else {
       return 'no'
     }
   
   }
-  
