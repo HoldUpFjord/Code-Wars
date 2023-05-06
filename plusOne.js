672735ed969a29eceb49c05f4fabdac6a95700ae
jackmonaco20 <jackmonaco20@yahoo.com>
2023-05-05 20:08:51 -0400

    complete leetcode solution

--- plusOne.js
+++ plusOne.js
@@ -19,3 +19,18 @@ var plusOne = function(digits) {
     return digits
     //return digits
 };
+
+//full solution 
+var plusOne = function(digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+         if (digits[i] !== 9) {
+             digits[i]++;
+             return digits;
+         }
+         digits[i] = 0;
+         if (i === 0) {
+             digits.unshift(1);
+             return digits;
+         }
+     }
+ };
