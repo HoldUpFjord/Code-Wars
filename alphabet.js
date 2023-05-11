dff5e23d99352224a54532ab513b0f6c824e6442
jackmonaco20 <jackmonaco20@yahoo.com>
2023-05-11 18:23:50 -0400

    .

---
+++ alphabet.js
@@ -0,0 +1,5 @@
+function position(letter){
+//Write your own Code!
+  let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  return `Position of alphabet: ${alphabet.indexOf(letter) + 1}`
+}
